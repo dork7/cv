b1615de8dacb7c373af06ab9a8aa15580134801e
Hamza Mehmood <hamza.mahmood@zho.gov.ae>
2023-05-07 22:28:49 +0400

    fab animation

--- src/components/Buttons/FloatingButton.jsx
+++ src/components/Buttons/FloatingButton.jsx
@@ -23,7 +23,23 @@ const FloatingButton = () => {
     const { links } = useUserDataSet();
 
     return (
-        <>
+        <motion.div
+            initial="hidden"
+            animate="visible"
+            variants={{
+                hidden: {
+                    // scale: 0.8,
+                    opacity: 0,
+                },
+                visible: {
+                    scale: 1,
+                    opacity: 1,
+                    transition: {
+                        delay: 0.4,
+                    },
+                },
+            }}
+        >
             <Fab
                 mainButtonStyles={{
                     color: '#FF6347',
@@ -81,7 +97,7 @@ const FloatingButton = () => {
                     </Action>
                 )}
             </Fab>
-        </>
+        </motion.div>
     );
 };
 
