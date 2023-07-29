34374c11f6512685970ed2f214c6840f39d99bb7
Hamza Mehmood <hamza.mahmood@zho.gov.ae>
2023-07-29 18:28:01 +0400

    email added

--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -1,25 +1,44 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import './contactform.styles.css'
+import Loader from './../Loader'
 const ContactForm = () => {
+
+    const emailRef = useRef()
+    const messageRef = useRef()
+    const [isLoading, setIsLoading] = useState(false)
+
+    const onSubmitHandler = async (e) => {
+        e.preventDefault()
+
+        setIsLoading(true)
+        const sendMail = await fetch('https://node-app-tau.vercel.app/v1/mail', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            body: JSON.stringify({
+                email: 'hamzameh122@gmail.com',
+                mailBody: `Email :${emailRef.current.value} Message: ${messageRef.current.value}`
+            })
+        })
+        setIsLoading(false)
+    }
     return (
         <div><div className="form-container">
-            <div className="logo-container">
-                Forgot Password
-            </div>
-
-            <form className="form">
+            <form className="form" onSubmit={onSubmitHandler} id="form">
                 <div className="form-group">
                     <label for="email">Email</label>
-                    <input type="text" id="email" name="email" placeholder="Enter your email" required="" />
+                    <input ref={emailRef} type="text" id="email" name="email" placeholder="Enter your email" required="" />
+                </div>
+                <div className="form-group">
+                    <textarea ref={messageRef} type="text" id="message" name="message" rows="4" placeholder="Message here" required="" />
                 </div>
 
-                <button className="form-submit-btn" type="submit">Send Email</button>
+                <button className="form-submit-btn" type="submit">   {isLoading ? <Loader /> : "Send Mail"}</button>
             </form>
 
-            <p className="signup-link">
-                Don't have an account?
-                <a href="#" className="signup-link link"> Sign up now</a>
-            </p>
+
         </div></div>
     )
 }
