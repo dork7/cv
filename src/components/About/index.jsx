c2e98eb9663f2527421989ff2d1605f99260a5d0
Hamza Mehmood <hamza.mahmood@zho.gov.ae>
2023-08-12 13:02:11 +0400

    first section fixed

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -3,47 +3,50 @@ import './about.styles.css'
 const AboutComponent = ({ userData }) => {
     const { aboutMe, aboutMe2, experience } = userData
     return (
-        <div className='about-container'>
-            <div className="section">
-                <p>
-                    {aboutMe}
-                </p>
-            </div>
+        <div className="parent-container">
 
-            <div>
-                <p style={{
-                    textAlign: "center"
-                }}>
-                    Experience
-                </p>
-                <div className='experience-section'>
-                    {
-                        experience.map(({
-                            designation,
-                            company,
-                            place,
-                            description,
-                            year
-                        }) => {
-                            return (<div key={company} className="exp-card">
-                                <div>
-                                    {company}
-                                    <p>
-                                        {designation}
-                                    </p>
-                                    <p>
-                                        {place}
-                                    </p>
-                                    <p>
-                                        {year}
-                                    </p>
-                                </div>
-                            </div>)
-                        })
-                    }
+            <div className='about-container'>
+                <div className="section">
+                    <p>
+                        {aboutMe}
+                    </p>
                 </div>
-            </div>
-        </div >
+
+                <div>
+                    <p style={{
+                        textAlign: "center"
+                    }}>
+                        Experience
+                    </p>
+                    <div className='experience-section'>
+                        {
+                            experience.map(({
+                                designation,
+                                company,
+                                place,
+                                description,
+                                year
+                            }) => {
+                                return (<div key={company} className="exp-card">
+                                    <div>
+                                        {company}
+                                        <p>
+                                            {designation}
+                                        </p>
+                                        <p>
+                                            {place}
+                                        </p>
+                                        <p>
+                                            {year}
+                                        </p>
+                                    </div>
+                                </div>)
+                            })
+                        }
+                    </div>
+                </div>
+            </div >
+        </div>
     )
 }
 
